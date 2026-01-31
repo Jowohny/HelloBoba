3a44a6feaee2abdd21a0a9c3da87c9c9e08b1aa9
Jowohny <vujohny109238@gmail.com>
2026-01-30 23:41:29 -0800

    base tailwind setup

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,8 @@
 <template>
-  <NuxtRouteAnnouncer />
-  <NuxtPage />
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+
+		<p class="text-black font-black text-5xl tracking-widest block text-center m-12">How are you?</p>
+  </div>
 </template>
