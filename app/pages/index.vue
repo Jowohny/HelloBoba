263916a30fc77c475436edc317d76f0a938525a7
Jowohny <vujohny109238@gmail.com>
2026-02-09 20:15:51 -0800

    added hero background
    yes ai image sue me, gotta wait till the boss gets some good images

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,29 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import gsap from 'gsap';
 
+const heroImage = ref(null);
+
+onMounted(() => {
+  const tl = gsap.timeline({ delay: 3.8 });
+
+  tl.from(heroImage.value, {
+    scale: 1.15,
+    filter: 'blur(20px)',
+    duration: 2.5,
+    ease: "expo.out"
+  });
+});
 </script>
 
 <template>
-	<div></div>
+  <div class="relative h-screen w-full flex items-end justify-center overflow-hidden bg-zinc-950">
+    <div ref="heroImage" class="absolute inset-0 z-0">
+      <NuxtPicture 
+        src="/HelloHero.png" 
+        class="h-full w-full object-cover"
+        :img-attrs="{ class: 'h-full w-full object-cover opacity-70' }"
+      />
+    </div>
+  </div>
 </template>
