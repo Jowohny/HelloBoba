acd85e5b4be5db12b1dcbaafa0db4c7ac0617e49
Jowohny <vujohny109238@gmail.com>
2026-06-04 17:45:13 -0700

    google review data

---
+++ app/data/reviews.ts
@@ -0,0 +1,121 @@
+import type { Review } from '~/types'
+
+export const reviews: Review[] = [
+	{
+		pfp: 'https://lh3.googleusercontent.com/a-/ALV-UjWiFfpOc9nsCcCI97_K0ntJYjx5NEVbNmjYETYfc21YuGhkhUgC=s64-c-rp-mo-ba12-br100',
+		name: 'AC9',
+		message: 'This place gives you free boba?! So let’s see if they’re pretty good eh or ew. They have a promo right now so if you leave them a google review or follow them on insta you can get 10% off your order. Service was chill and fast and I got their butterfly mango, horchata smoothie, and strawberry matcha to try. They even have some extra desserts so I got a matcha macaron to try too. It had lots of matcha flavor so it was a bit seaweedy. Nothing too special. The butterfly mango drink was really pretty and I gave it to my friend and she really enjoyed it.',
+		images: [
+			'https://lh3.googleusercontent.com/grass-cs/ANxoTn2jQjZZh4id5DoaCKZOWQ9pvZFmq1she3nUVDWIdw_JMlN5OE3aJdIzxToqe1oeD9QhJDqDTen5s4YJ95YEIvYkEyFKEUd0b1U-NymGgB7q36DoGjRMH-PNpNoz6tuI5Dpwkh34_pRtCgk=s250-p-k',
+			'https://lh3.googleusercontent.com/grass-cs/ANxoTn1XO33NsyCmeEz0yb84A-TZotelgC3-Me38-aDDxtJEmC5lFKKdZKf7SnliOzorDo-1FrKP8Lybz38mZuFyN9sARC54E_qer9S6eTC6TE-TDLPKXrrTCnCMWKXucDwK9ZBlOsstvB_kpwI=s250-p-k',
+			'https://lh3.googleusercontent.com/grass-cs/ANxoTn1IdtutWJYIdk8gY6HzMxOdBEgQCdtqe6L9EHD2DnwzPzRgxLU9LEz83soQ7tJ8cs6FEfd7J2qBzDFivSvGsQNY65rqodn4uWJ3SScESdHgGPS63LcxxnFXOXizlUR6-OMSIqWUzhVBWYJR=s250-p-k'
+		],
+		link: 'https://share.google/ZaUJY18CEsIezjNdH'
+	},
+	{
+		pfp: 'https://lh3.googleusercontent.com/a-/ALV-UjVdsZQA-U17ccA1I7x4W2qYh7ogUeXD6e1dgD_M1nDytraeep2NDw=s64-c-rp-mo-ba12-br100',
+		name: 'GiGi Fuentes',
+		message: 'The Boba texture and taste is excellent in my opinion. That alone deserves a 5 star. Happy hours (buy one get one 50% off) M-F only 4-8pm. Got a refreshing drink because I didn’t want to stay awake at night. Definitely going to try their coffee and tea next time.',
+		images: [
+			'https://lh3.googleusercontent.com/grass-cs/ANxoTn156DLo3SwiCC4DLnGUSWRG7NSqrr5gYykH1m1uICc8QkqPLe9NQcfezIoRjuH_6XLkHWH0Czbb8k-4qFVnpiIbEPDQ50b8R4UxAO_yuwjBEKsUisp1YTkMm3ImvkPajjJPFwW9=s250-p-k',
+			'https://lh3.googleusercontent.com/grass-cs/ANxoTn1l8aY6RLMl1ozrvIDJCJhU2QIARRNNb0kpm56dUBAEYdV6fBew9CJ7Zw5KhqIZHdaDJwkEnElAmjo5LffCQZaFCxaFqeuSDyXuzoxZ0GMSDJlnG4drd-7RkMKugxaRfEydEKZz=s250-p-k',
+			'https://lh3.googleusercontent.com/grass-cs/ANxoTn3_kK6I0On23Nob2EEavXFfHgMbTwyAm38WqPnq6TOLRU-JqlUeUAOS35kIj1IPhVmAjTo3PGd-B0uBZe1t-teQ2YqXkO4FIGxfB_kR6WM97jbF_MTeEd_2hO7a71KqczmeWf5F=s250-p-k'
+		],
+		link: 'https://share.google/KYBllz3AE6xSgfWRn'
+	},
+	{
+		pfp: 'https://lh3.googleusercontent.com/a-/ALV-UjUGpBK6ASwaukzSGNI2QOjfu9ROw6i8rEljGyLJGc09tZQOnQE=s64-c-rp-mo-ba12-br100',
+		name: 'Danger Waffle',
+		message: 'Hello Boba is the perfect spot to grab a refreshing boba drink, whether it’s a hot summer day or you’re just in the mood for something delicious. From the moment I first stepped in, I loved the stylish and inviting atmosphere. The cashier was friendly, adding to the warm vibe, and I was pleasantly surprised to find charming little ornaments for sale in the center of the shop. For my first order, I went with a large Strawberry Matcha Latte, no boba—and it did not disappoint! If you\'re looking for a great place to enjoy a quality drink, I highly recommend stopping by.',
+		images: [],
+		link: 'https://share.google/Fn9nZKeFciWQ1wsyU'
+	},
+	{
+		pfp: 'https://lh3.googleusercontent.com/a/ACg8ocLJO4zjvwBJaMPEWJclCXCuvWZ9HA4c_ARNh8TGtchoutUMAfFA=s64-c-rp-mo-ba12-br100',
+		name: 'Alexander Tran',
+		message: 'Can’t believe I never checked this place out and I live so close. Milk tea is on par with my go to "half &half" and "Ding tea", but this place has more variety and aesthetically pleasing drinks for the gram. This place has such a simple cute interior decor. I will start coming here more often, love supporting local businesses.',
+		images: [
+			'https://lh3.googleusercontent.com/grass-cs/ANxoTn3hwBfbbjx4kVDoQn9WGSJyy76Wx1YgPUphT8isJcrzYYi6gd63rhluVhzZwGNgU9cpe8IzK-Kw2YseDk0jZw3SQdLKIZjdHHmfe0ublwYFmY_1FzMS9lAmVYl5cQNkK9mW1l_o=s250-p-k',
+			'https://lh3.googleusercontent.com/grass-cs/ANxoTn12m7L53uTjTV2inbxBvKOUBjuV-B_xQdLYGxljjomDlhD3eJvxH35sjEtRBi79Geyc8Oz9_8iYAZBtgfQFLmlS5Ggnhcwk3PCQXRcIIcMwYo4W2dAM7L4wx6B9Q8zyC4qZgGuLYQ=s250-p-k',
+			'https://lh3.googleusercontent.com/grass-cs/ANxoTn3BDFJH-U59C0F-1dAXr0salBSaH-IBhsASLszXaIR3n5LEYHn8TAP6X8NKkMWDUwGJLTgi7MUQIQZOOJCV9cKVcOqg_EKAdbJTdfy2wzu3L52PBYHk5Vg1Kk9XOwHdc_Ua-_wq=s250-p-k'
+		],
+		link: 'https://share.google/7EQFEOkW8xbtKFHdp'
+	},
+	{
+		pfp: 'https://lh3.googleusercontent.com/a-/ALV-UjVddFpu8RLZuFYPNIka67dS2g4IssbgdzjZ_wAvZyQOpYDjkBo-=s64-c-rp-mo-ba12-br100',
+		name: 'Camel Lama',
+		message: 'I stumbled upon this location by accident during my lunch break from work and I love their drinks, price rating, and kiwi bird mascot! 💖✨ Would definitely visit more often. 😃 Stamp cards are available to get one free drink after 7 stamps and you can opt to get their merch if you fill up 2 cards. (Merch are: boba pop socket and boba night lamp!)',
+		images: [
+			'https://lh3.googleusercontent.com/grass-cs/ANxoTn0rYTNhKXVktx7K6-02bnOQwzZodBvpef9UWT-rEBA1Sa7UI3G-ltIxVG4y7o-uGzVIjTWtrCClwwexUGR-D-9RrZ2fRpyXgKfCPEhHI1bu5tdT8w_-8p49hxvnkOCx3D1gp--p=s250-p-k',
+			'https://lh3.googleusercontent.com/grass-cs/ANxoTn3lswPxtMSVwjEijMKYf-4rqqrEpJZ0z-SLF4hGdDsyHXyfoDbAdry7w_aisrnM8LRlAbFkrDPQ0AU3iOkt56sOqnLK1wkiJmbOLnmkXP6kCUAwDHVlRMSEnio4Q-M7WscbPrVS=s250-p-k',
+			'https://lh3.googleusercontent.com/grass-cs/ANxoTn0noHu2G7FgC53Ak2SSl-amn9pjX6XyO62PO6tjv4_T8FM1RX2t1fSXUSGGSKCVZRR7sSd-PYcmpGilsgVBUvYNxl6yP70TDO-d-9aVeu0hzm1zkV4Cep3KtJ2TEMrpasDxMKtM=s250-p-k'
+		],
+		link: 'https://share.google/kTyBUrIgsiFz6oo31'
+	},
+	{
+		pfp: 'https://lh3.googleusercontent.com/a-/ALV-UjUnHZrrkyiqZvUuXUlJHfGE6IPwQpHwq3MuyQOWd9u7feEPZZuBiQ=s64-c-rp-mo-ba12-br100',
+		name: 'Ben Evardome',
+		message: 'We did a major order of 100 boba milk tea drinks for our daughter’s birthday party and we had no issues with this store. The employees were extremely helpful and transporting all the drinks, straws, etc to my car and everything was neatly organized. I highly recommend this place. Also the employees are super friendly, thank you Hello Boba for making our daughter’s birthday a memorable one. :)',
+		images: [],
+		link: 'https://share.google/osceXdUa5ndZWpfp6'
+	},
+	{
+		pfp: 'https://lh3.googleusercontent.com/a/ACg8ocJeVGrN7XxRhKcBlXr10EgyZlPko_xdm1A3wjE8uOg5QTU9dA=s64-c-rp-mo-ba12-br100',
+		name: 'Ny Lg',
+		message: 'Best kept secret in El Monte!!!! My new go-to boba place. Their prices are reasonable and the boba is perfect EVERY single time I go; I’ve been coming here for the last 4 months. Never is it overcooked. The boba is not sticky. And they are sweet. Great variety. Highly recommended to everyone!!!',
+		images: [
+			'https://lh3.googleusercontent.com/grass-cs/ANxoTn1_8-PEWWwlEBum2BHLCqIi0A_yULcYNqaBNPqTxAjqoYx_1cKg1Q6WF0obOqMp45-wf7s5WUDdUk8-dj1w17qNuZHJ4oVSLL7IoyH69bX9kjGtQeK2pSivVjpEoKbKEv36konZgA=s250-p-k'
+		],
+		link: 'https://share.google/0ptioOaWcG3VuX5jj'
+	},
+	{
+		pfp: 'https://lh3.googleusercontent.com/a/ACg8ocLv2AVLsJ9BHQERcdc0UOUwG8BSPEGm83IFHf4mPDMGUwdwRw=s64-c-rp-mo-ba12-br100',
+		name: 'Yoon Choi',
+		message: 'Cozy and cute interior, friendly and prompt service, and diverse menu.',
+		images: [
+			'https://lh3.googleusercontent.com/grass-cs/ANxoTn2LbZR_0z48vP2Qt8-i8yh8-i7tQgL83pqRHyyojKzAUcxUUqnR4_vY23XUdQ9eHVQ4TH6JsKkx-s9HafLg7g8HOuXUs44_b2gba0pBADJ1dFPPSB31tmPwTIYvvnBe-4bXC3tYp4YNmSJ7=s250-p-k',
+			'https://lh3.googleusercontent.com/grass-cs/ANxoTn0mQh5Mif29aJDZQTjuAyY6gwqASPxdbY_ajsj1wqKtQdACbE3QxgfBX_1WKJi0QVWUBafHxwrgtjhFwlk5XVa4DdSWbZZUKQ0UcE4Y82v0NCUvP2o2zEZxstGtmOnTMqsnRSxEjGBLBaOf=s250-p-k',
+			'https://lh3.googleusercontent.com/grass-cs/ANxoTn38Md4gO9DiqqzovO7BJ7ANKnSGP9jEUDhXJvGYm55HPwwYjcCdF336Epel1F8qdp83iKMzoMSSYCtqkM-_8NiE_GhhdSbFz3Fkd3Nxjikr1GUqNpyLjYdC1OtXKjhvgCGF5wbBr254yBTx=s250-p-k'
+		],
+		link: 'https://share.google/TBuBs3ZUFNbRURxyD'
+	},
+	{
+		pfp: 'https://lh3.googleusercontent.com/a-/ALV-UjXmT9cNAUBQw2dauLQEchlAdN6HCjk0--dzNuTY9Di5MX1hWj0=s64-c-rp-mo-ba12-br100',
+		name: 'Teresa Ramirez',
+		message: 'Tasty boba with original style. My son and I love coming here. Great variety of boba & tea flavors. Relaxing atmosphere & clean stylish decor.',
+		images: [
+			'https://lh3.googleusercontent.com/geougc-cs/AMG9lESBCvZN7QVHRpncVbPRLvX_8-_WXQeacLlUrYnjQgP_nJNQ2_0HZCljh679YHu8hZ2PozwyejFrIRoJdKc6Rfzmei8WbVvTQgFxQoYBSEENXRQR9R3bIAqI0bJKhAR6FH9VLnRg=s250-p-k',
+			'https://lh3.googleusercontent.com/grass-cs/ANxoTn39Op2jMgEfh2kkuwhUWRO_k7YvwPD3fT2_COUX19OmldvPML168simE9YYsSIUSkG8dUrOqrTHxTOpkhrSG3Vsny5LoX81fC1SQC0w2WXWRK42ojg0oxVT9CPPNQB7NFmmnNaj=s250-p-k',
+			'https://lh3.googleusercontent.com/grass-cs/ANxoTn2CIXWDCPREzz1mYRMlj5ssWejqA1pHK-cyz2AIF57g5CyiLNTnpq90aLe7kxe1fiqyD85342uSjWmRPyZJ5NMRmsjaVg9LvogKEQ0eDolxHOZf6KCbUVBIq7gmjcUuFULWHOBxig=s250-p-k'
+		],
+		link: 'https://share.google/6MT5g6WU7ZHx3A9b6'
+	},
+	{
+		pfp: 'https://lh3.googleusercontent.com/a/ACg8ocISVt-BgelhA3K2RCKwgresOVt7lOXIxJPKisRDhUnWJeORRg=s64-c-rp-mo-ba12-br100',
+		name: 'John Hua',
+		message: 'South El Monte has some of the best boba in the world. South El Monte is also home to a lot of Taiwanese boba supplies companies and import so that makes people who start boba shop businesses in the South El Monte an easy access to the materials and that makes it all thrive. When I am not getting the 7Leaves down the street in South El Monte, I would come here to study Mandarin or work on software engineering projects remotely. I think South El Monte has a thriving community of Asian and boba drink lovers. Hello Boba adds another great addition to that one. Another great option is Boba Time which is also right down the same street. I love to get boba and then head to the King Taco place and drink boba with my taco.',
+		images: [],
+		link: 'https://share.google/W473V4A3vI7CVwL6j'
+	},
+	{
+		pfp: 'https://lh3.googleusercontent.com/a/ACg8ocJ-GzIuA7Dhok81lD6_VOTs7KYvW2n_FaQUUZDXOrg-GYPRcQ=s64-c-rp-mo-br100',
+		name: 'Vincent Becerra',
+		message: 'Customer service was great and so was the drinks. Highly recommend. Tuan is the man make sure you stop by and said hi great attitude and a genuine guy.',
+		images: [
+			'https://lh3.googleusercontent.com/grass-cs/ANxoTn3NpbYqVdqwbzk9BfokkLIUvNJUgD6DuldfbqX9RT93fSeeLEDkSqnlhYQLZa1HkL9zFzx9XApOIZD3Tzobn1zQwcOFrsw1spznPEl95bybUCN3ZhKgVVCVWAxjXE0K6kZBrJZCboorhIMc=s250-p-k',
+		],
+		link: 'https://share.google/dvdw09LeK9vgxqT0w'
+	},
+	{
+		pfp: 'https://lh3.googleusercontent.com/a-/ALV-UjUwYCTouhQW_I6QK1yV1UjDftWEpvN6ppyeAwLSqXiNbLJNX3fe_g=s64-c-rp-mo-ba12-br100',
+		name: 'Rayeemon',
+		message: 'IG:@daysoffwithray Yo take your girl here! The boba is so good and the vibe is great! I love what they are building here. I got the strawberry matcha late. Big recommendations on that one',
+		images: [
+			'https://lh3.googleusercontent.com/grass-cs/ANxoTn1NnfGrOW_s-lEKYCSrDczSDBICHau7duYKlskZtKwW1fDxSInDbPjL-_BU0VtiMUrM0my6alNOjDeqlbGc68RDFCQG6gue5GD_WJz4wF3dTLboI70AKJTEb4AEzxKKViMmVS0=s250-p-k',
+			'https://lh3.googleusercontent.com/grass-cs/ANxoTn0OooYkAAMjK9f0alQTNy6BwngUfWyDmuuKxjqSh7E3w6unYUt3dEJHh7IWhgUZX_CosB1U10YWCDcKsTMsh06ho22takT3dsb7FZoMSVL5L8QV5zHm1uKTJpA8gPJ03R_dEtzH=s250-p-k',
+			'https://lh3.googleusercontent.com/grass-cs/ANxoTn3mCe0hgv8QR0HPhkcA4yhO72fizNeeRHXSsNgsnn3zw8O-1fYfjg-tP16SGXriqGoSra6GqB2D86S8W_1NUry9JTfpeu2Z9G1kbRzEW768W4VT01VSbnLDvSSzUhcirosl0cWL=s250-p-k'
+		],
+		link: 'https://share.google/NaH0Nec4lfNtaVyDy'
+	}
+]
+
